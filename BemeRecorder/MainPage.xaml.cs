048fec5ba948e4da31d90ca01a68658cdd3e255f
UWPanda <uwp_anda@outlook.com>
2015-10-22 12:28:40 +0200

    initial commit

---
+++ BemeRecorder/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Sensors;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace BemeRecorder
+{
+    public sealed partial class MainPage : Page
+    {
+        private ProximitySensor sensor;
+        private DeviceWatcher watcher;
+        private bool isRecording;
+        private bool isInitialized;
+        private MediaCapture mediaCapture;
+        private static Timer timer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            watcher = DeviceInformation.CreateWatcher(ProximitySensor.GetDeviceSelector());
+            watcher.Added += OnProximitySensorAdded;
+            watcher.Start();
+            timer = new Timer(TimerCallBack, null, Timeout.Infinite, 4000);
+            await InitializeCameraAsync();
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+            if (mediaCapture == null)
+            {
+                // get camera device (back camera preferred)
+                var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
+
+                if (cameraDevice == null)
+                {
+                    Debug.WriteLine("no camera device found");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                mediaCapture = new MediaCapture();
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await mediaCapture.InitializeAsync(settings);
+                    isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Debug.WriteLine("access to the camera denied");
+                }
+            }
+        }
+
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            // get all camera devices
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            // get camera by panel
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+
+            // if there is no camera on the desired panel - return default camera
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+
+        private void OnProximitySensorAdded(DeviceWatcher sender, DeviceInformation device)
+        {
+            if (null == sensor)
+            {
+                ProximitySensor foundSensor = ProximitySensor.FromId(device.Id);
+                if (null != foundSensor)
+                {
+                    sensor = foundSensor;
+                    sensor.ReadingChanged += Sensor_ReadingChanged;
+                }
+                else
+                {
+                    Debug.WriteLine("device has no proximity sensor");
+                }
+            }
+        }
+        private async void Sensor_ReadingChanged(ProximitySensor sender, ProximitySensorReadingChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                ProximitySensorReading reading = e.Reading;
+                if (null != reading)
+                {
+                    if (reading.IsDetected)
+                    {
+                        //hide status bar
+                        if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+                        {
+                            var statusBar = StatusBar.GetForCurrentView();
+                            await statusBar.HideAsync();
+                        }
+                        //enable timer
+                        timer.Change(4000, 4000);
+                        Overlay_RecordingTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                        Overlay_RecordingTextBlock.Text = "Recording...";
+                        Overlay_RecordingTextBlock.Visibility = Visibility.Visible;
+                        Overlay_Grid.Visibility = Visibility.Visible;
+                        await Task.Delay(1000);
+                        Overlay_RecordingTextBlock.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        if (isRecording)
+                        {
+                            //disable timer
+                            timer.Change(Timeout.Infinite, 4000);
+                            Overlay_RecordingTextBlock.Foreground = new SolidColorBrush(Color.FromArgb(100, 255, 255, 255));
+                            Overlay_RecordingTextBlock.Text = "Done...";
+                            Overlay_RecordingTextBlock.Visibility = Visibility.Visible;
+                            await Task.Delay(1000);
+                            Overlay_Grid.Visibility = Visibility.Collapsed;
+                            await StopRecordingAsync();
+                            isRecording = false;
+                        }
+                        else
+                        {
+                            timer.Change(Timeout.Infinite, 4000);
+                            Overlay_RecordingTextBlock.Foreground = new SolidColorBrush(Color.FromArgb(100, 255, 255, 255));
+                            Overlay_RecordingTextBlock.Text = "Canceled...";
+                            Overlay_RecordingTextBlock.Visibility = Visibility.Visible;
+                            await Task.Delay(1000);
+                            Overlay_Grid.Visibility = Visibility.Collapsed;
+                            isRecording = false;
+                        }
+                    }
+                }
+            });
+        }
+
+        private async Task StartRecordingAsync()
+        {
+            try
+            {
+                //create video file in picture library
+                var videoFile = await KnownFolders.PicturesLibrary.CreateFileAsync("Beme.mp4", CreationCollisionOption.GenerateUniqueName);
+
+                var encodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto);
+                await mediaCapture.StartRecordToStorageFileAsync(encodingProfile, videoFile);
+                isRecording = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private async Task StopRecordingAsync()
+        {
+            isRecording = false;
+            await mediaCapture.StopRecordAsync();
+        }
+        private async void TimerCallBack(object state)
+        {
+            if (!isRecording)
+            {
+                await StartRecordingAsync();
+                isRecording = true;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# BemeRecorder
-Beme-Like video recording on UWP
+![picture Header](https://raw.githubusercontent.com/UWPanda/TabbedPivot/master/Images/GitHub_Header.png "Header")
+
+#BemeRecorder
+Similar to the Beme app on iOS BemeRecorder uses the ProximitySensor of the device to trigger video recording in UWP.
+
+The start and stop of the video recording is controlled within this method - it's called every time the reading of the ProximitySensor has changed.
+```csharp
+//Get readings from ProximitySensor
+ private async void Sensor_ReadingChanged(ProximitySensor sender, ProximitySensorReadingChangedEventArgs e)
+{
+	
+	ProximitySensorReading reading = e.Reading;
+	if (null != reading)
+	{
+		if (reading.IsDetected)
+		{
+	
+		}
+	}
+}
+```
+
+Once a recording is started the video is stored on the file system in the photos directory. Maybe stitching multiple clips together is comming in a tutorial in the future - this one is just about triggering recordings via the ProxmitySensor.
+```csharp
+	var videoFile = await KnownFolders.PicturesLibrary.CreateFileAsync("Beme.mp4", CreationCollisionOption.GenerateUniqueName);
+
+	var encodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto);
+	await mediaCapture.StartRecordToStorageFileAsync(encodingProfile, videoFile);
+```
+
+#Screenshot
+![picture screenshot](https://raw.githubusercontent.com/UWPanda/TabbedPivot/master/Images/Screenshot.png "Screenshot")
+
+#Author
+
+[nor0x](https://github.com/nor0x) for [UWPanda](https://uwpanda.azurewebsites.net)
+
+#License
+
+BemeRecorder is available under the MIT license. See the LICENSE file for more info.
+
